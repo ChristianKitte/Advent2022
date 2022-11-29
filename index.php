98c30f97eb77197bbb641e02c6c1aa2e41ddbf23
Christian Kitte <chkitte@web.de>
2022-11-29 19:26:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ endfor; ?>
     <script><?php include('js.php') ?></script>
 </head>
 <body>
-<h1><a href="https://selfhtml.org"><?= $title ?></a></h1>
+<h1><a href="https://advent.ckitte.de/"><?= $title ?></a></h1>
 <ol> <?php
     for ($k = 0; $k < sizeof($listitems); $k++) : ?>
         <li><?= $listitems[$k] ?></li> <?php
